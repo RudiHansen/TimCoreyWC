ac7c3fa64dc3f39cd1f87dbcb677b8e41a6618d8
Rudi Hansen <Rudi.Hansen@outlook.dk>
2019-01-31 23:59:15 +0100

    First draft of code, this works but its not pretty

---
+++ 2019/05-SWAPI/Data/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_SWAPI.Data
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Height { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\t" + 
+                   $"Gender: {Gender}\t" +
+                   $"Height: {Height}";
+        }
+    }
+}
--- 2019/05-SWAPI/Program.cs
+++ 2019/05-SWAPI/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using _05_SWAPI.Data;
+using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +12,45 @@ namespace _05_SWAPI
 {
     class Program
     {
-        static void Main(string[] args)
+        static HttpClient client = new HttpClient();
+
+        static async Task<Person> GetPersonAsync(string personId)
         {
+            Person person = null;
+            HttpResponseMessage response = await client.GetAsync($"people/{personId}");
+            if (response.IsSuccessStatusCode)
+            {
+                person = await response.Content.ReadAsAsync<Person>();
+                
+            }
+            return person;
+        }
+
+        static void Main()
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("https://swapi.co/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                // Get the product
+                Person person = await GetPersonAsync("1");
+                Console.WriteLine($"{person.ToString()}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
